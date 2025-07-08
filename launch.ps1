4c6d252f2523ee5fe7aa40129f4021391500ec24
glomo <badjokeresult@gmail.com>
2025-07-08 12:35:53 +0300

    finishing

--- launch.ps1
+++ launch.ps1
@@ -1,6 +1,11 @@
-& ".\loki-upgrader.exe"
+Start-Process -FilePath ".\loki-upgrader.exe"
+
 Copy-Item .\bizone-landscape.yar .\signature-base\yara
 Copy-Item .\c2-bizone-landscape.txt .\signature-base\iocs
 Copy-Item .\filename-bizone-landscape.txt .\signature-base\iocs
+Copy-Item .\c2-kaspersky-asian-apt.txt .\signature-base\iocs
+Copy-Item .\filename-kaspersky-asian-apt.txt .\signature-base\iocs
+Copy-Item .\hash-kaspersky-asian-apt.txt .\signature-base\iocs
+
 Get-Content .\keywords.txt >> .\signature-base\iocs\keywords.txt
 Start-Process -FilePath ".\loki.exe" -ArgumentList "--allhds"
